a6c21f029592a05b2147a817401b082f2345d747
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2023-07-16 15:36:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function scrollToSection(sectionId) {
+    var target = document.querySelector(sectionId); // Get the target element
+    if (target) {
+        var offsetTop = target.offsetTop; // Get the target element's top offset
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth' // Smooth scroll behavior
+        });
+    }
+}
