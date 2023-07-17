4a99e7ee8d78a3a8e5611227c9e2de1c1fc7409b
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2023-07-16 22:19:42 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-
-// function scrollToSection(sectionId) {
-//     var target = document.querySelector(sectionId); // Get the target element
-//     if (target) {
-//         var offsetTop = target.offsetTop; // Get the target element's top offset
-//         window.scrollTo({
-//             top: offsetTop,
-//             behavior: 'smooth' // Smooth scroll behavior
-//         });
-//     }
-// }
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+}
