31834eabf4f32d91493ac12a0be1cfc9907688be
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2023-07-19 15:58:43 -0500

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -4,3 +4,11 @@ const dropdownMenu = document.getElementById('dropdownMenu');
 hamburgerMenu.addEventListener('click', () => {
   dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
 });
+
+// Event listener to close the dropdown menu when clicking outside of it
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target !== dropdownMenu && target !== hamburgerMenu) {
+      dropdownMenu.style.display = 'none';
+    }
+  });
